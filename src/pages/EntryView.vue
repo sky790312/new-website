0af9a6ca100f5623a88eab628b9630bbebdd4a0c
kevinhu <sky790312yks@gmail.com>
2017-11-17 02:16:54 +0800

    [refactor] entryView page

--- src/pages/EntryView.vue
+++ src/pages/EntryView.vue
@@ -1,59 +1,49 @@
 <template>
   <div id="entry-view">
-    <a-scene fog="type: exponential; color:#000">
-      <a-sky matrix rotation="-5 -10 0"></a-sky>
-      <!-- <a-entity>
-        <a-animation attribute="rotation"
-          delay="0"
-          dur="1000"
-          to="0 10 360"
-          repeat="indefinite"
-          easing="linear">
-        </a-animation>
-        <a-sphere id="sphere1" position="0 3 0" radius="0.3" color="white"></a-sphere>
-      </a-entity> -->
+    <a-scene
+      :fog="scene.fog">
+      <a-sky
+        matrix>
+      </a-sky>
       <a-text
-        :value="welcomeWebsite"
-        position="-2.2 2 0"
-        width="7">
+        :value="welcomeTitle.text"
+        :position="welcomeTitle.position"
+        :width="welcomeTitle.width">
       </a-text>
       <a-text
-        :value="entryHint"
-        position="-3.3 1.4 0"
-        width="7">
+        :value="welcomeDesc.text"
+        :position="welcomeDesc.position"
+        :width="welcomeDesc.width">
       </a-text>
       <a-plane
-        position="0 0 0"
-        rotation="-90 0 0"
-        width="4"
-        height="4"
-        color="#f8f8f8">
+        :position="plane.position"
+        :rotation="plane.rotation"
+        :width="plane.width"
+        :height="plane.height"
+        :color="plane.color">
         <a-animation
-          attribute="rotation"
-          delay="0"
-          dur="5000"
-          to="-90 0 360"
-          repeat="indefinite"
-          easing="linear">
+          :attribute="plane.animation.attribute"
+          :dur="plane.animation.duration"
+          :to="plane.animation.to"
+          :repeat="plane.animation.repeat"
+          :easing="plane.animation.easing">
         </a-animation>
       </a-plane>
-      <a-entity position="0 -0.8 3.8">
-        <a-camera>
-          <a-cursor
-            color="white"
-            geometry="primitive: ring; radiusOuter: 0.03; radiusInner: 0.02"
-            listener>
-            <a-animation
-                begin="click"
-                easing="ease-in"
-                attribute="scale"
-                from="1 1 1"
-                to="1000 1000 1000"
-                dur="1000">
-            </a-animation>
-          </a-cursor>
-        </a-camera>
-      </a-entity>
+      <a-camera :position="camera.position">
+        <a-cursor
+          :color="cursor.color"
+          :geometry="cursor.geometry"
+          listener>
+          <a-animation
+            :begin="cursor.animation.begin"
+            :easing="cursor.animation.easing"
+            :attribute="cursor.animation.attribute"
+            :from="cursor.animation.from"
+            :to="cursor.animation.to"
+            :dur="cursor.animation.duration">
+          </a-animation>
+        </a-cursor>
+      </a-camera>
     </a-scene>
   </div>
 </template>
@@ -68,8 +58,48 @@ export default {
 
   data () {
     return {
-      welcomeWebsite: this.$t('entryView.welcomeWebsite'),
-      entryHint: this.$t('entryView.entryHint')
+      scene: {
+        fog: 'type: exponential; color:#444'
+      },
+      welcomeTitle: {
+        text: this.$t('entryView.welcomeWebsite'),
+        position: '-2.2 2 0',
+        width: '7'
+      },
+      welcomeDesc: {
+        text: this.$t('entryView.entryHint'),
+        position: '-3.3 1.4 0',
+        width: '7'
+      },
+      plane: {
+        position: '0 0 0',
+        rotation: '-90 0 0',
+        width: 4,
+        height: 4,
+        color: '#f8f8f8',
+        animation: {
+          attribute: 'rotation',
+          to: '-90 0 360',
+          repeat: 'indefinite',
+          easing: 'linear',
+          duration: 5000
+        }
+      },
+      camera: {
+        position: '0 -0.8 3.8'
+      },
+      cursor: {
+        color: 'white',
+        geometry: 'primitive: ring; radiusOuter: 0.03; radiusInner: 0.02',
+        animation: {
+          attribute: 'scale',
+          begin: 'click',
+          from: '1 1 1',
+          to: '1000 1000 1000',
+          easing: 'ease-in',
+          duration: '1000'
+        }
+      }
     }
   },
 
@@ -86,24 +116,24 @@ export default {
           dependencies: ['draw'],
           init () {
             this.draw = this.el.components.draw
-            this.cnvs = this.draw.canvas
-            const columns = this.cnvs.width / fontSize
+            this.canvas = this.draw.canvas
             this.drops = []
-            for (let x = 0; x < columns; x++) {
-              this.drops[x] = 1
+            const columns = this.canvas.width / fontSize
+            for (let i = 0; i < columns; i++) {
+              this.drops[i] = 1
             }
-            this.ctx = this.draw.ctx
           },
 
           tick () {
-            this.ctx.fillStyle = 'rgba(0,0,0,0.05)'
-            this.ctx.fillRect(0, 0, this.cnvs.width, this.cnvs.height)
-            this.ctx.fillStyle = '#0F0'
-            this.ctx.font = `${fontSize} px helvetica`
+            const ctx = this.draw.ctx
+            ctx.fillStyle = 'rgba(0,0,0,0.05)'
+            ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+            ctx.fillStyle = '#0F0'
+            ctx.font = `${fontSize} px helvetica`
             for (let i = 0; i < this.drops.length; i++) {
               const txt = chars[Math.floor(Math.random() * chars.length)]
-              this.ctx.fillText(txt, i * fontSize, this.drops[i] * fontSize)
-              if (this.drops[i] * fontSize > this.cnvs.height &&
+              ctx.fillText(txt, i * fontSize, this.drops[i] * fontSize)
+              if (this.drops[i] * fontSize > this.canvas.height &&
                 Math.random() > 0.975) {
                 this.drops[i] = 0
               }
@@ -113,18 +143,17 @@ export default {
           }
         })
       }
+    },
 
-      const self = this
+    initListener () {
       if (typeof (AFRAME.components.listener) === 'undefined') {
+        const self = this
         AFRAME.registerComponent('listener', {
           init () {
-            this.el.addEventListener('click', evt => {
-              setTimeout(() => {
-                self.$emit('shouldShowEntryView', false)
-                // self.setIsEntryView(false)
-                // self.$router.push('/home')
-              }, 500)
-            })
+            this.el.addEventListener('click', evt =>
+              setTimeout(() =>
+                self.$emit('shouldShowEntryView', false), 500)
+            )
           }
         })
       }
@@ -133,6 +162,7 @@ export default {
 
   beforeMount () {
     this.initMatrixView()
+    this.initListener()
   }
 }
 </script>
