bed6ea6bf9a35a7ad6e3975af2870d1a3884ee78
kevinhu <sky790312yks@gmail.com>
2018-01-04 02:27:59 +0800

    completed TypeWrite component unit test & refactor

--- test/unit/specs/StrokeText.spec.js
+++ test/unit/specs/StrokeText.spec.js
@@ -14,14 +14,14 @@ describe('StrokeText', () => {
   it('should render correct number of dash', () => {
     const text = 'test'
     const vm = generateStrokeText(text)
-    const strokeText = vm.$el.querySelectorAll('#stroke-text .text')
-    expect(strokeText.length).to.equal(vm.dashCount)
+    const strokeTexts = vm.$el.querySelectorAll('#stroke-text .text')
+    expect(strokeTexts.length).to.equal(vm.dashCount)
   })
 
   it('should use props and rendor to capital text', () => {
     const text = 'test'
     const vm = generateStrokeText(text)
-    const strokeText = vm.$el.querySelectorAll('#stroke-text .text')
+    const strokeText = vm.$el.querySelector('#stroke-text text')
     expect(strokeText.textContent.trim()).to.equal(text.toUpperCase())
   })
 })
