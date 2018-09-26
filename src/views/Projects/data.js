355a8857abbb898aa529a274c75b4cc85a367ee8
kevin.hu <>
2018-09-26 18:38:50 +0800

    feat: completed projects page prototype

--- src/views/Projects/data.js
+++ src/views/Projects/data.js
@@ -1,131 +1,157 @@
 export const PROJECTS = [{
   name: '179',
-  type: 'aite',
+  company: 'aite',
+  skills: ['angularjs', 'jquery', 'nodejs'],
   linkUrl: '',
   imageUrl: require('~assets/projects/aite/179.jpg')
 }, {
   name: 'aiteOfficial',
-  type: 'aite',
+  company: 'aite',
+  skills: ['wordpress'],
   linkUrl: 'http://www.aite.com.tw',
   imageUrl: require('~assets/projects/aite/aite-official.jpg')
 }, {
   name: 'cpcOfficial',
-  type: 'aite',
+  company: 'aite',
+  skills: ['wordpress'],
   linkUrl: '',
   imageUrl: require('~assets/projects/aite/cpc-official.jpg')
 }, {
   name: 'lejetbcOfficial',
-  type: 'aite',
+  company: 'aite',
+  skills: ['wordpress'],
   linkUrl: '',
   imageUrl: require('~assets/projects/aite/lejetbc-official.jpg')
 }, {
   name: 'lejetbc',
-  type: 'aite',
+  company: 'aite',
+  skills: ['angularjs', 'jquery', 'nodejs'],
   linkUrl: '',
   imageUrl: require('~assets/projects/aite/lejetbc.jpg')
 }, {
   name: 'spotmeDashboard',
-  type: 'istaging',
+  company: 'istaging',
+  skills: ['angularjs', 'jquery', 'nodejs'],
   linkUrl: '',
   imageUrl: require('~assets/projects/aite/spotme-dashboard.jpg')
 }, {
   name: 'spotme',
-  type: 'aite',
+  company: 'aite',
+  skills: ['angularjs', 'jquery', 'nodejs'],
   linkUrl: '',
   imageUrl: require('~assets/projects/aite/spotme.jpg')
 }, {
   name: 'spotrainer',
-  type: 'aite',
+  company: 'aite',
+  skills: ['wordpress'],
   linkUrl: '',
   imageUrl: require('~assets/projects/aite/spotrainer.jpg')
 }, {
   name: 'couponTaiwan',
-  type: 'eztravel',
+  company: 'eztravel',
+  skills: ['java play', 'knoukout', 'jquery'],
   linkUrl: 'https://activity.eztravel.com.tw/taiwan',
   imageUrl: require('~assets/projects/eztravel/coupon-taiwan.jpg')
 }, {
   name: 'homepage',
-  type: 'eztravel',
+  company: 'eztravel',
+  skills: ['java play', 'knoukout', 'jquery'],
   linkUrl: 'https://www.eztravel.com.tw',
   imageUrl: require('~assets/projects/eztravel/homepage.jpg')
 }, {
   name: 'ucar',
-  type: 'eztravel',
+  company: 'eztravel',
+  skills: ['java play', 'knoukout', 'jquery'],
   linkUrl: '',
   imageUrl: require('~assets/projects/eztravel/ucar.jpg')
 }, {
   name: 'ucarApp',
-  type: 'eztravel',
+  company: 'eztravel',
+  skills: ['nodejs', 'jquery'],
   linkUrl: '',
   imageUrl: require('~assets/projects/eztravel/ucar-app.jpg')
 }, {
   name: '3dDollhouse',
-  type: 'istaging',
+  company: 'istaging',
+  skills: ['vue', 'aframe', 'nodejs'],
   linkUrl: 'https://www.istaging.com/dollhouse',
   imageUrl: require('~assets/projects/istaging/3d-dollhouse.jpg')
 }, {
   name: 'furniture',
-  type: 'istaging',
+  company: 'istaging',
+  skills: ['angularjs', 'nodejs'],
   linkUrl: '',
   imageUrl: require('~assets/projects/istaging/furniture.jpg')
 }, {
   name: 'furnitureDashboard',
-  type: 'istaging',
+  company: 'istaging',
+  skills: ['angularjs', 'nodejs'],
   linkUrl: 'http://www.staging.com.tw/furniture_dashboard_us',
   imageUrl: require('~assets/projects/istaging/furniture-dashboard.jpg')
 }, {
   name: 'vreditorCn',
-  type: 'istaging',
+  company: 'istaging',
+  skills: ['vue', 'aframe', 'nodejs'],
   linkUrl: 'https://vreditor.istaging.com.cn',
   imageUrl: require('~assets/projects/istaging/vreditor-cn.jpg')
 }, {
   name: 'vreditorUs',
-  type: 'istaging',
+  company: 'istaging',
+  skills: ['vue', 'aframe', 'nodejs'],
   linkUrl: 'https://vreditor.istaging.com',
   imageUrl: require('~assets/projects/istaging/vreditor-us.jpg')
 }, {
   name: 'vreditorSdk',
-  type: 'istaging',
+  company: 'istaging',
+  skills: ['vue', 'aframe', 'nodejs'],
   linkUrl: 'https://github.com/iStaging/vreditor-sdk',
   imageUrl: require('~assets/projects/istaging/vreditor-sdk.jpg')
 }, {
   name: 'vrviewer',
-  type: 'istaging',
+  company: 'istaging',
+  skills: ['vue', 'krpano', 'nodejs'],
   linkUrl: 'https://livetour.istaging.com/261da9fb-f700-4cf9-aee6-39d96eb1afae',
   imageUrl: require('~assets/projects/istaging/vrviewer.jpg')
 }, {
   name: 'vrviewerSdk',
-  type: 'istaging',
+  company: 'istaging',
+  skills: ['vue', 'krpano', 'nodejs'],
   linkUrl: 'https://github.com/iStaging/vrviewer-sdk',
   imageUrl: require('~assets/projects/istaging/vrviewer-sdk.jpg')
 }, {
   name: 'vrsdkDoc',
-  type: 'istaging',
+  company: 'istaging',
+  skills: ['vue', 'vuepress'],
   linkUrl: 'https://evs-doc-test.istaging.com.cn',
   imageUrl: require('~assets/projects/istaging/vrsdk-doc.jpg')
 }, {
   name: 'vrportal',
-  type: 'istaging',
+  company: 'istaging',
+  skills: ['vue', 'nodejs'],
   linkUrl: 'https://portal.istaging.com/',
   imageUrl: require('~assets/projects/istaging/vrportal.jpg')
 }, {
   name: 'vrlive',
-  type: 'istaging',
+  company: 'istaging',
+  skills: ['vue', 'aframe', 'nodejs'],
   linkUrl: '',
   imageUrl: require('~assets/projects/istaging/vrlive.jpg')
 }, {
   name: 'oldWebsite',
-  type: 'others',
+  company: 'others',
+  skills: ['jquery', 'nodejs'],
   linkUrl: 'https://sky790312.herokuapp.com',
   imageUrl: require('~assets/projects/others/old-website.jpg')
 }, {
   name: 'requestAnimationProfolio',
-  type: 'others',
+  company: 'others',
+  skills: ['pure js'],
   linkUrl: '',
   imageUrl: require('~assets/projects/others/requestanimation-profolio.jpg')
 }, {
   name: 'updownrightleftProfolio',
-  type: 'others',
+  company: 'others',
+  skills: ['jquery'],
   linkUrl: 'https://sky790312.github.io/full-screen-presentation-website/presentation.html',
   imageUrl: require('~assets/projects/others/updownrightleft-profolio.jpg')
 }]
--- src/views/Projects/index.vue
+++ src/views/Projects/index.vue
@@ -1,32 +1,85 @@
 <template>
   <div
     id="projects">
-    <div id="sudoku-demo" class="demo">
-      <h1>Projects</h1>
-      <button @click="shuffle">
-        Shuffle
-      </button>
-      <transition-group
-        name="project"
-        tag="div"
-        class="container">
+    <h1>
+      {{ $t('menus.projects') }}
+    </h1>
+    <nav
+      class="main-menu-container">
+      <menu
+        class="control-main-menus">
+        <li 
+          v-for="(menuState, mainMenu) in mainMenusState"
+          :key="mainMenu"
+          :class="['control-main-menu', {
+            'active' : menuState,
+            'filtering': activeFilters[mainMenu].length
+          }]"
+          @click="setMainMenusState(mainMenu, menuState)">
+          {{ mainMenu }}
+        </li>
+        <transition
+          name="fade">
+          <li
+            v-show="activeFilters.companies.length || activeFilters.skills.length"
+            class="control-main-menu clear-all"
+            @click="clearAllFilters">
+            Clear all
+          </li>
+        </transition>
+      </menu>
+      <menu>
+        <li
+          v-for="(activeFilterArray, activeFilter) in activeFilters"
+          :key="activeFilter">
+          {{ activeFilter }}:
+          <span
+            v-for="filter in activeFilterArray"
+            :key="filter">
+            {{ filter }}
+          </span>
+        </li>
+      </menu>
+    </nav>
+
+    <transition-group
+      name="sub-menu-container"
+      tag="section"
+      class="sub-menu-container"
+      :style="subMenuContainerStyle">
+      <menu 
+        v-for="(mainFilterArray, mainFilter) in filters"
+        :key="mainFilter"
+        class="control-sub-menus"
+        v-show="mainMenusState[mainFilter]"
+        :ref="mainFilter">
+        <li 
+          v-for="(filterState, filter) in mainFilterArray"
+          :key="filter"
+          :class="['control-sub-menu', { 'active': filterState }]"
+          @click="setFilter(mainFilter, filter)">
+          {{ filter }}
+        </li>
+      </menu>
+    </transition-group>
+
+    <transition-group 
+      name="project"
+      tag="div"
+      class="project-container">
+      <div
+        v-for="project in filteredProjects"
+        :key="project.name"
+        class="project">
+        <img
+          :src="project.imageUrl"
+          class="project-image">
         <div 
-          v-for="project in projects"
-          :key="project.name"
-          class="project">
-          <!-- <a
-            class="project-title">
-            {{ project.name }}
-          </a> -->
-          <img
-            :src="project.imageUrl"
-            class="project-image">
-          <div class="project-title">
-            {{ project.name }}
-          </div>
+          class="project-title">
+          {{ project.name }}
         </div>
-      </transition-group>
-    </div>
+      </div>
+    </transition-group>
   </div>
 </template>
 
@@ -34,17 +87,6 @@
 import {
   PROJECTS
 } from '@/views/Projects/data'
-
-function shuffle (sourceArray) {
-  for (var i = 0; i < sourceArray.length - 1; i++) {
-    var j = i + Math.floor(Math.random() * (sourceArray.length - i))
-
-    var temp = sourceArray[j]
-    sourceArray[j] = sourceArray[i]
-    sourceArray[i] = temp
-  }
-  return sourceArray
-}
 
 export default {
   name: 'Projects',
@@ -54,17 +96,101 @@ export default {
 
   data () {
     return {
-      projects: PROJECTS
+      projects: PROJECTS,
+      subMenuContainerStyle: {
+        height: ''
+      },
+      filters: {
+        companies: {},
+        skills: {}
+      },
+      mainMenusState: {
+        companies: false,
+        skills: false
+      }
     }
   },
 
   computed: {
+    filteredProjects () {
+      return this.projects.filter(project => {
+        if (this.activeFilters.companies.length && !~this.activeFilters.companies.indexOf(project.company)) {
+          return false
+        }
+
+        return !this.activeFilters.skills.length || this.activeFilters.skills.every(skill => ~project.skills.indexOf(skill))
+      })
+    },
+
+    activeMainMenu () {
+      return Object.keys(this.mainMenusState).reduce((accumulator, currentMenu, index) => {
+        return (this.mainMenusState[currentMenu])
+            ? currentMenu
+            : accumulator
+      }, '')
+    },
+
+    activeFilters () {
+      return {
+        companies: Object.keys(this.filters.companies).filter(company => this.filters.companies[company]),
+        skills: Object.keys(this.filters.skills).filter(skill => this.filters.skills[skill])
+      }
+    }
+  },
+
+  watch: {
+    activeMainMenu (oldVal, newVal) {
+      if (oldVal === newVal) {
+        return
+      }
+
+      this.$nextTick(() => {
+        if (!this.$refs[oldVal]) {
+          this.subMenuContainerStyle.height = 0
+          return
+        }
+
+        this.subMenuContainerStyle.height = (this.$refs[oldVal][0])
+          ? `${this.$refs[oldVal][0].clientHeight + 50}px`
+          : 0
+      })
+    }
   },
 
   methods: {
-    shuffle: function () {
-      this.projects = [...shuffle(this.projects)]
+    setFilter (mainMenu, subMenu) {
+      if (mainMenu === 'companies') {
+        this.filters[mainMenu][subMenu] = !this.filters[mainMenu][subMenu]
+      } else {
+        this.clearFilter(mainMenu, subMenu, this.filters[mainMenu][subMenu])
+      }
+    },
+
+    clearFilter (mainMenu, subMenu, currentFilter) {
+      Object.keys(this.filters[mainMenu]).forEach(filterSubMenu => {
+        this.filters[mainMenu][filterSubMenu] = (!currentFilter && subMenu === filterSubMenu)
+      })
+    },
+
+    clearAllFilters () {
+      Object.keys(this.filters).forEach(this.clearFilter)
+    },
+
+    setMainMenusState (currentMenu, currentMenuState) {
+      Object.keys(this.mainMenusState).forEach(mainMenu => {
+        this.mainMenusState[mainMenu] = (!currentMenuState && mainMenu === currentMenu)
+      })
     }
+  },
+
+  beforeMount () {
+    PROJECTS.forEach(project => {
+      this.$set(this.filters.companies, project.company, false)
+
+      project.skills.forEach(skill => {
+        this.$set(this.filters.skills, skill, false)
+      })
+    })
   }
 }
 </script>
@@ -75,11 +201,120 @@ export default {
 #projects {
   color: $white
 
-  .container {
+  .main-menu-container {
+    display: flex
+    justify-content: space-between
+    align-items: center
+    white-space: nowrap
+    margin: 0 1rem
+    padding: 2rem 0.5rem 1rem
+    border-bottom: 1px solid $light-gray
+
+    .control-main-menus {
+      display: flex
+    }
+
+    .control-main-menu {
+      position: relative
+      margin-left: 1rem
+      text-transform: capitalize
+      z-index: 1
+      cursor: pointer
+
+      &::after {
+        content: '\00d7'
+        display: inline-block
+        color: transparent
+        width: 0.5rem
+        font-weight: 400
+        transform: scale(0)
+        transition: transform .15s ease-in-out
+      }
+
+      &.clear-all {
+        color: #f68185
+      }
+
+      &.filtering {
+        &::after {
+          content: '\2022'
+          transform: scale(1)
+          color: $green2
+        }
+      }
+    }
+  }
+
+  .sub-menu-container {
+    position: relative
+    height: 0
+    overflow: hidden
+    transition: height 350ms
+
+    &::after {
+      content: ''
+      position: absolute
+      bottom: 0
+      left: 0
+      width: 100%
+      height: 1rem
+      background-image: linear-gradient(to top, $white, rgba($white, 0))
+    }
+
+    &-enter,
+    &-leave-to {
+      opacity: 0
+    }
+
+    &-leave,
+    &-enter-to {
+      opacity: 1
+    }
+
+    &-enter-active,
+    &-leave-active {
+      position: absolute
+      width: 100%
+      transition: opacity 200ms ease-in-out
+    }
+
+    &-enter-active {
+      transition-delay: 100ms
+    }
+  }
+
+  .control-sub-menus {
+    padding: 0 1rem
     display: flex
     flex-wrap: wrap
-    // width: 105px;
-    // margin-top: 10px;
+    align-items: flex-start
+
+    .control-sub-menu {
+      margin-top: 0.5rem
+      margin-right: 0.5rem
+      padding: 0.25rem 0.5rem
+      border: 1px solid $light-gray
+      border-radius: 6px
+      font-size: 0.8rem
+      line-height: 1.35
+      cursor: pointer
+      transition: all 275ms
+
+      &:hover {
+        border-color: $green2
+      }
+
+      &.active {
+        color: white
+        border-color: $green2
+        background-color: $green2
+      }
+    }
+  }
+
+  .project-container {
+    display: flex
+    flex-wrap: wrap
   }
 
   .project {
@@ -87,16 +322,14 @@ export default {
     display: flex
     align-items: center
     justify-content center
+    color: $white
     margin: 20px
     width: 200px
     height: 150px
     cursor: pointer
     overflow: hidden
-    transition: all .3s
 
     &:hover {
-      padding: 5px
-
       .project-image {
         opacity: .3
       }
@@ -119,10 +352,29 @@ export default {
       align-items: center
       opacity: 0
     }
-  }
 
-  .project-move {
-    transition: transform 1s
+    &-move { 
+      transition: all 600ms ease-in-out 50ms 
+    }
+
+    &-enter-active {
+      transition: all 300ms ease-out
+    }
+
+    &-leave-active {
+      transition: all 200ms ease-in
+      position: absolute
+      z-index: 0
+    }
+
+    &-enter,
+    &-leave-to {
+      opacity: 0
+    }
+
+    &-enter {
+      transform: scale(.9)
+    }
   }
 }
 
