cfba712493ea27fb28a66f8df582a121e7f8ec37
kevin.hu <>
2018-10-08 15:54:03 +0800

    feat: auto get all modules file key to vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import language from '@/store/modules/language'
-import speechBubble from '@/store/modules/speechBubble'
-// import entryView from '@/store/modules/entryView'
-
 Vue.use(Vuex)
 
-const store = new Vuex.Store({
-  modules: {
-    language,
-    speechBubble
-    // entryView
-  },
+import modules from '@/store/modules'
+
+export const store = new Vuex.Store({
+  modules,
   strict: true
 })
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,9 @@
+export const camelize = (str) => {
+  return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function (letter, index) {
+    return index === 0 ? letter.toLowerCase() : letter.toUpperCase()
+  }).replace(/\s+/g, '')
+}
+
 export const requiredParam = (param) => {
   const requiredParamError = new Error(
    `Required parameter, "${param}" is missing.`
