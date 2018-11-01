6824bdf3b7d61b43468ac1751e0a776bb6bc5e85
kevin.hu <>
2018-11-01 17:12:35 +0800

    feat: add ITimeline component story

---
+++ src/stories/components/ITimeline.story.js
@@ -0,0 +1,58 @@
+/* eslint-disable */
+import { storiesOf } from '@storybook/vue'
+import Centered from '@storybook/addon-centered';
+import { withKnobs, object } from '@storybook/addon-knobs/vue';
+
+import ITimeline from '@/components/ITimeline'
+
+const stories = storiesOf('ITimeline')
+stories.addDecorator(withKnobs)
+stories.addDecorator(Centered)
+
+stories
+  .add('default', () => {
+    const item1 = object('1', {
+      key: 1,
+      text: 'item 1'
+    }, 'items-group')
+    const item2 = object('2', {
+      key: 2,
+      text: 'item 2'
+    }, 'items-group')
+    const item3 = object('3', {
+      key: 3,
+      text: 'item 3'
+    }, 'items-group')
+    const items = [item1, item2, item3]
+    return {
+      components: { ITimeline },
+      template: `
+        <div>
+          <p>
+            Pass array object(must has key parameter) to ITimeline component, and use scope-slot to get item's data.
+          </p>
+          <p>
+            It will divide the width equally with each item and it's RWD with 768px breakpoint.
+          </p>
+          <i-timeline
+            :items="items">
+            <template
+              slot-scope="{ item }">
+              <p>
+                {{item}}
+              </p>
+              <p>
+                {{ item.text }}
+              </p>
+            </template>
+          </i-timeline>
+        </div>
+      `,
+      data () {
+        return {
+          items
+        }
+      }
+    }
+  }
+)
