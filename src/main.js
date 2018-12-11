29afb1a03ce4fe585f6da026364fef8c84792a56
kevin.hu <>
2018-12-11 18:18:33 +0800

    feat: add console color text, meta information

--- src/main.js
+++ src/main.js
@@ -18,7 +18,11 @@ Vue.mixin({
   methods: globalMixinMethods
 })
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
+
+let consoleText = `welcome to KevinHu's website!`
+let consoleStyle = `font-size: 20px; background-color: red; font-weight: bold; color: white; text-shadow: 0 0 1em black, 0 0 1em black, 0 0 1em black, 0 0 1em black, 0 0 1em black; padding: 3px 5%; border: .1em solid rgba(0,0,0,.4); border-radius: 1em;`
+console.log('%c' + consoleText, consoleStyle)
 
 /* eslint-disable */
 export const app = new Vue({
