965ca034a1c8f4597b838532ba3ed19b463ffecc
kevin.hu <kevin.hu@kevinhude-iMac.local>
2018-12-12 14:09:07 +0800

    Adding files.

--- server.js
+++ server.js
@@ -5,5 +5,6 @@ var serveStatic = require('serve-static')
 var app = express()
 app.use(serveStatic(__dirname + '/dist')) // eslint-disable-line
 var port = process.env.PORT || 5000
+app.set(port)
 app.listen(port)
 console.log('server started ' + port)
