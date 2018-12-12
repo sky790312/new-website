c191fa192f2e64187bd0e47be62199ed9541f673
kevin.hu <>
2018-12-12 18:22:53 +0800

    fix: fix express server route to vue route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.set(port)
 
 app.use(express.static('dist'))
 
+app.get('*', function (req, res) {
+  res.sendfile('dist/index.html')
+})
+
 app.listen(port, function () {
   console.log('Example app listening on port 5000!')
 })
